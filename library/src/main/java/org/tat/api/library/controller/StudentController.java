3123ead6ee51f7501084f01e651742d397f46dbd
Kara Satish Kumar <karasatishkumar@gmail.com>
2015-12-29 18:26:06 +0530

    pagination and sorting

--- library/src/main/java/org/tat/api/library/controller/StudentController.java
+++ library/src/main/java/org/tat/api/library/controller/StudentController.java
@@ -1,8 +1,11 @@
 package org.tat.api.library.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.QueryHint;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.tat.api.library.model.Address;
@@ -26,9 +30,36 @@ public class StudentController {
 
 	@RequestMapping(method = RequestMethod.GET, headers = "Accept=application/json", produces = { "application/json" })
 	@ResponseBody
-	public List<Student> getStudents() {
+	public List<Student> getStudents(
+			@RequestParam(value = "offset", defaultValue = "0") Integer offset,
+			@RequestParam(value = "limit", defaultValue = "20") Integer limit,
+			@RequestParam(value = "sort", required = false) String sort) {
+		List<Sort> sortConfigs = null;
 
-		List<Student> students = service.getStudents();
+		if (sort != null && !sort.isEmpty()) {
+			sortConfigs = new ArrayList<Sort>();
+			List<String> sorts = Arrays.asList(sort.split("\\s*,\\s*"));
+			Sort sortConfig;
+			for (String s : sorts) {
+				sortConfig = new Sort();
+				if (s.indexOf("-", 0) >= 0) {
+					sortConfig.setField(s.substring(1));
+					sortConfig.setSortType(SortType.DESC);
+				} else {
+					sortConfig.setField(s);
+					sortConfig.setSortType(SortType.ASC);
+				}
+				sortConfigs.add(sortConfig);
+			}
+		}
+
+		if (sortConfigs != null && sortConfigs.size() > 0)
+			for (Sort sort2 : sortConfigs) {
+				System.out.println("Sort Field = " + sort2.getField());
+				System.out.println("Sort Type = " + sort2.getSortType());
+		}
+
+		List<Student> students = service.getStudents(offset, limit, sortConfigs);
 		return students;
 	}
 
@@ -40,7 +71,7 @@ public class StudentController {
 		Student student = service.getStudent(studentId);
 		return student;
 	}
-	
+
 	@RequestMapping(value = "/{studentId}/address", method = RequestMethod.GET, headers = "Accept=application/json", produces = { "application/json" })
 	@ResponseBody
 	public Address getStudentAddress(@PathVariable int studentId,
@@ -49,7 +80,7 @@ public class StudentController {
 		Address address = service.getStudentAddress(studentId);
 		return address;
 	}
-	
+
 	@RequestMapping(value = "/{studentId}/resources", method = RequestMethod.GET, headers = "Accept=application/json", produces = { "application/json" })
 	@ResponseBody
 	public Set<Resource> getStudentResources(@PathVariable int studentId,
